6b407f7b8aafb6e3a834a7db27086a5bf79e86ea
‘Aspetto33 <1760906445@qq.com>
2021-12-08 22:15:40 +0800

    增加client请求server过程的实现（lab0-0.5）

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,7 +8,21 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
+    TCPSocket tcpSocket;
 
+    tcpSocket.connect(Address(host,"http"));
+
+    tcpSocket.write("GET"+path+"HTTP/1.1\r\nHost:"+host+"\r\n\r\n");
+
+    //tell service not to wait
+    tcpSocket.shutdown(SHUT_WR);
+
+    while(!tcpSocket.eof()){
+        cout<<tcpSocket.read();
+    }
+
+
+    tcpSocket.close();
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
